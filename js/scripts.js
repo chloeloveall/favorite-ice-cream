242daaafebcd273b9b3c6a0c2e64143bfc374739
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-24 16:54:00 -0800

    Add for each loop to display ice creams

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  const iceCreamsArray = ['Birthday Cake', 'Salted Caramel', 'Chocolate', 'Vanilla'];
+
+iceCreamsArray.forEach(function(iceCream) {
+  $('#whatILove').append('I love ' + iceCream + '! ');
+});
+
+});
