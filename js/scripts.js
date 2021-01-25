7257f132d2bec2504cde5043a18d3e1bbc9701aa
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-24 16:54:40 -0800

    Refactor to correct indent

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
   const iceCreamsArray = ['Birthday Cake', 'Salted Caramel', 'Chocolate', 'Vanilla'];
 
-iceCreamsArray.forEach(function(iceCream) {
+  iceCreamsArray.forEach(function(iceCream) {
   $('#whatILove').append('I love ' + iceCream + '! ');
-});
+  });
 
 });
